b2038e8f62ad2208f6d5ca23d8efd135fcbb9cf1
Matt Blodgett <thailandmatt@gmail.com>
2023-11-30 22:37:55 -0700

    Day 1 - took way too long

--- src/aoc/Year2023_Day01.java
+++ src/aoc/Year2023_Day01.java
@@ -2,8 +2,10 @@ package aoc;
 
 import java.util.ArrayList;
 
+import static java.lang.Integer.parseInt;
+
 // Put the return type of the generator in Base<>
-public class Year2023_Day01 extends Base<ArrayList<Integer>> {
+public class Year2023_Day01 extends Base<ArrayList<String>> {
 
     // The generator's role is to turn the input file into something usable by part 1 and part 2.
     void generator() {
@@ -12,38 +14,92 @@ public class Year2023_Day01 extends Base<ArrayList<Integer>> {
         // read it line by line
         for(String line: this.input.split("\n")){
             // for this one we just want an arraylist of numbers
-            this.processed.add(Integer.parseInt(line));
+            this.processed.add(line);
         }
     }
 
     void part1() {
-        int lastValue = this.processed.get(0);
-
-        int counter = 0;
-        for(int value: this.processed){
-            if(value > lastValue){
-                counter += 1;
+        int count = 0;
+        for (String line: this.processed) {
+            int first = -1;
+            int last = -1;
+            for (int i = 0; i < line.length(); i++) {
+                if (Character.isDigit(line.charAt(i))) {
+                    first =  Character.getNumericValue(line.charAt(i));
+                    break;
+                }
             }
-            lastValue = value;
+
+            for (int i = line.length() - 1; i >= 0; i--) {
+                if (Character.isDigit(line.charAt(i))) {
+                    last =  Character.getNumericValue(line.charAt(i));
+                    break;
+                }
+            }
+
+            count += parseInt(String.valueOf(first) + String.valueOf(last));
         }
 
         // print out the result
-        System.out.printf("Part 1: %d %n", counter);
+        System.out.printf("Part 1: %d\n", count);
     }
 
     void part2() {
-        int counter = 0;
-        for(int i = 3; i < this.processed.size(); i++){
-            int first = this.processed.get(i-3) + this.processed.get(i-2) + this.processed.get(i-1);
-            int second = this.processed.get(i-2) + this.processed.get(i-1) + this.processed.get(i);
 
+        // print out the result
+        String[] validWords =
+                {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-            if(second > first){
-                counter += 1;
+        int count = 0;
+        for (String line: this.processed) {
+
+            int first = -1;
+            int last = -1;
+            for (int i = 0; i < line.length(); i++) {
+                if (Character.isDigit(line.charAt(i))) {
+                    first =  Character.getNumericValue(line.charAt(i));
+                    break;
+                } else {
+                    boolean found = false;
+                    for (int j = 0; j < validWords.length; j++) {
+                        var word = validWords[j];
+                        if (i + word.length() <= line.length()) {
+                            if (line.substring(i, i + word.length()).equals(word)) {
+                                first = j + 1;
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (found) break;
+                }
             }
+
+            for (int i = line.length() - 1; i >= 0; i--) {
+                if (Character.isDigit(line.charAt(i))) {
+                    last =  Character.getNumericValue(line.charAt(i));
+                    break;
+                } else {
+                    boolean found = false;
+                    for (int j = 0; j < validWords.length; j++) {
+                        var word = validWords[j];
+                        if (i - word.length() >= -1) {
+                            if (line.substring(i - word.length() + 1, i + 1).equals(word)) {
+                                last = j + 1;
+                                found = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (found) break;
+                }
+            }
+
+            var x = parseInt(String.valueOf(first) + String.valueOf(last));
+            count += x;
         }
 
-        // print out teh result
-        System.out.printf("Part 2: %d %n", counter);
+        // print out the result
+        System.out.printf("Part 2: %d\n", count);
     }
 }
